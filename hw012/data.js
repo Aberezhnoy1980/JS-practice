dfd6ab56aec655c7f207c7229c502bfa79fc72d6
Aleksandr Berezhnoy <alex@iMac-IMAC.local>
2025-05-14 09:48:44 +0300

    hw12 code added

---
+++ hw012/data.js
@@ -0,0 +1,46 @@
+const data = `
+[
+    {
+        "id": 1,
+        "price": 111,
+        "description": "Awesome Shirt! Spring season collection!",
+        "img": "./img/p1.jpg",
+        "title": "Shirt"
+    },
+    {
+        "id": 2,
+        "price": 222,
+        "description": "Awesome pants! Summer collection!",
+        "img": "./img/p2.png",
+        "title": "pants"
+    },
+    {
+        "id": 3,
+        "price": 333,
+        "description": "Awesome cap! Autumn collection!",
+        "img": "./img/p3.png",
+        "title": "cap"
+    },
+    {
+        "id": 4,
+        "price": 444,
+        "description": "Awesome jacket! Winter collection!",
+        "img": "./img/p4.png",
+        "title": "jacket"
+    },
+    {
+        "id": 5,
+        "price": 555,
+        "description": "Awesome t-shirt! Summer collection!",
+        "img": "./img/p5.png",
+        "title": "t-shirt"
+    },
+    {
+        "id": 6,
+        "price": 676,
+        "description": "Awesome shorts! Spring collection!",
+        "img": "./img/p6.png",
+        "title": "shorts"
+    }
+]
+`;
